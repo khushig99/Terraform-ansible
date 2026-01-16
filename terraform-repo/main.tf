953b35c58fe5bea893994ec0f6f139eeb7b16d5d
Khushi Gupta <104582212+khushig99@users.noreply.github.com>
2026-01-16 15:14:00 +0530

    .

--- terraform-repo/main.tf
+++ terraform-repo/main.tf
@@ -5,3 +5,10 @@ module "my_vm" {
     machine_type = "e2-medium"
     zone = "us-central1-a"
 }
+
+module "my_bucket" {
+    source = "./modules/bucket"
+
+    project_id = "getteam"
+    bucket_name = "bucket-frm-module"
+}
---
+++ terraform-repo/modules/bucket/variable.tf
@@ -0,0 +1,32 @@
+variable "project_id" {
+    type = string
+    default = getteam
+}
+variable "region" {
+    type = string
+    default = "us-central1"
+}
+
+variable "bucket_name" {
+    type = string
+}
+
+variable "location" {
+    type = string
+    default = "US"
+}
+
+variable "storage_class" {
+    type = string
+    default = "STANDARD"
+}
+
+variable "versioning" {
+    type = bool
+    default = true
+}
+
+variable "force_destroy" {
+    type = bool
+    default = false
+}
